cb9ede5d556462f33936ed1d5d46c045240865fb
Jacky Zhao <jzhao4342@gmail.com>
2025-03-02 11:54:38 -0500

    Fixed seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,18 @@
-user = User.create!(email: "test@test.com", password: "Testing123")
+ActiveRecord::Base.transaction do
+  user = User.create!(email: "user@test.com", password: "Testing123")
 
-tag1 = Tag.create!(name: "Tag1", order: 1, user: user)
-tag2 = Tag.create!(name: "Tag2", order: 1, user: user)
+  tag1 = ClientTag.create!(name: "Tag1", order: 1, user: user)
+  tag2 = ClientTag.create!(name: "Tag2", order: 1, user: user)
 
-(0..9).each do |counter|
-  client = Client.create!(user: user, first_name: "First #{counter}", last_name: "Last #{counter}", email: "client#{counter}@test.com", phone: "416000000#{counter}")
+  (0..9).each do |counter|
+    client = Client.create!(user: user, first_name: "First #{counter}", last_name: "Last #{counter}", email: "client#{counter}@test.com", phone: "416000000#{counter}")
 
-  if counter % 2 == 0
-    client.update!(gender: "male")
-    client_tag = ClientTag.create!(client: client, tag: tag1)
-  else
-    client.update!(gender: "female")
-    client_tag = ClientTag.create!(client: client, tag: tag2)
+    if counter % 2 == 0
+      client.update!(gender: "male")
+      client_tag = ClientClientTag.create!(client: client, client_tag: tag1)
+    else
+      client.update!(gender: "female")
+      client_tag = ClientClientTag.create!(client: client, client_tag: tag2)
+    end
   end
 end
