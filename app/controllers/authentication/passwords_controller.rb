eec6b02ada1bf50118938cc8a9e9e206ad0fa1ea
Jacky Zhao <jzhao4342@gmail.com>
2025-03-02 11:52:33 -0500

    Added password and sessions controller.

---
+++ app/controllers/authentication/passwords_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Authentication::PasswordsController < Devise::PasswordsController
+
+end
---
+++ app/controllers/authentication/sessions_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Authentication::SessionsController < Devise::SessionsController
+
+end
