60257a0aaa439a41804d30f82371ed178919b93b
Jacky Zhao <jzhao4342@gmail.com>
2025-03-02 21:24:06 -0500

    Added respond_with override in SessionsController.

--- app/controllers/authentication/sessions_controller.rb
+++ app/controllers/authentication/sessions_controller.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 class Authentication::SessionsController < Devise::SessionsController
+  def respond_with(resource, _opts = {})
+    puts "Jacky sessions resource"
+    puts resource
 
+    if resource.errors.any?
+      render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
+    else
+      @user = resource
+      render 'users/show'
+    end
+  end
 end
